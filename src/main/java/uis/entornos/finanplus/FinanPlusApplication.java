bff4cf98dc470122ef1c0f10b5f884079fd04175
Javier Alean <javieralean2006@gmail.com>
2026-09-19 19:26:20 -0500

    feat: Integración a estructura básica de backend spring boot

---
+++ src/main/java/uis/entornos/finanplus/FinanPlusApplication.java
@@ -0,0 +1,13 @@
+package uis.entornos.finanplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uis/entornos/finanplus/FinanPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package uis.entornos.finanplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
